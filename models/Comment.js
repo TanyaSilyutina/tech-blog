acb7be4a48a6fbc6a9bd545c344bd07b0f217222
Tanya Silyutina <48433669+TanyaSilyutina@users.noreply.github.com>
2023-06-17 15:41:00 -0700

    created models and relationships

---
+++ models/Comment.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+   
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
---
+++ models/index.js
@@ -0,0 +1,17 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+
+User.hasMany(Post);
+
+Post.belongsTo(User);
+Post.hasMany(Comment);
+
+Comment.belongsTo(User);
+
+module.exports = {
+    User,
+    Post,
+    Comment,
+};
